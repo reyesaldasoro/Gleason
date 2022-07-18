2843d6eff8e80b162cb1e30da00529d8d9878ab6
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2022-07-18 14:46:39 +0100

    add detection of innerWhite and innerTissue

--- CODE/detectBackground.m
+++ CODE/detectBackground.m
@@ -1,4 +1,4 @@
-function [backgroundMask,innerWhite] = detectBackground(currentImage)
+function [backgroundMask,innerWhite,innerTissue,meanBackground,meanTissue] = detectBackground(currentImage)
 
 %%
 % Find the background through a combination of saturation and morphological
@@ -26,8 +26,36 @@ strelElement2        = strel('disk',50);
 H_E_chromatic_highSat = H_E_chromatic.*(1-data_lowSat);
 % H_E_chromatic_highSatL = bwlabel(H_E_chromatic_highSat);
 H_E_chromatic_highSat2 =  imopen(imclose(H_E_chromatic_highSat,strelElement1),strelElement2);
- backgroundMask      = (H_E_chromatic_highSat2)| black_background;
-innerWhite = 0;
+backgroundMask      = (H_E_chromatic_highSat2)| black_background;
+
+%% Inner white 
+rCh                 = currentImage(:,:,1);
+gCh                 = currentImage(:,:,2);
+bCh                 = currentImage(:,:,3);
+rCh_1               = mean(rCh(backgroundMask==1));
+rCh_0               = mean(rCh(backgroundMask==0));
+gCh_1               = mean(gCh(backgroundMask==1));
+gCh_0               = mean(gCh(backgroundMask==0));
+bCh_1               = mean(bCh(backgroundMask==1));
+bCh_0               = mean(bCh(backgroundMask==0)); 
+
+meanBackground      = [rCh_0 gCh_0 bCh_0 ]; 
+meanTissue          = [rCh_1 gCh_1 bCh_1 ]; 
+strelElement3       = strel('disk',15);
+backgroundMask2     = imerode(backgroundMask,strelElement3);
+%backgroundMask3     = imdilate(1-backgroundMask2,ones(9));
+innerWhite1         = bwmorph(backgroundMask2.*(1-H_E_chromatic_highSat),'majority');
+[innerWhite2,numW]  = bwlabel(innerWhite1);
+innerWhite2_P       = regionprops(innerWhite2,'area');
+% remove small specks and those that touch background
+%notTouchBackground  = setdiff((1:numW), unique((backgroundMask3).*innerWhite2));
+largeSpecks         = find([innerWhite2_P.Area]>10);
+%largestSpecks       = find([innerWhite2_P.Area]>10);
+%innerWhite3         = ismember(innerWhite2,intersect(largeSpecks,notTouchBackground));
+
+innerWhite3         = ismember(innerWhite2,largeSpecks);
+innerWhite          = innerWhite3;
+innerTissue         = (1-innerWhite3).*backgroundMask2;
 %%
 % % Label to keep large regions
 % data_lowSat_L       = bwlabel(data_lowSat1);
