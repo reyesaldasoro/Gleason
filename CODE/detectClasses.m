0fe657baf94e2d88fac05f381a22219bc72da34f
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2022-07-20 17:34:26 +0100

    add definitions for N, G, S

--- CODE/detectClasses.m
+++ CODE/detectClasses.m
@@ -1,4 +1,4 @@
-function [backgroundMask,innerWhite,innerTissue,meanBackground,meanTissue] = detectBackground(currentImage)
+function [backgroundMask,innerWhite,innerTissue,meanBackground,meanTissue] = detectClasses(currentImage)
 
 %%
 % Find the background through a combination of saturation and morphological
@@ -31,7 +31,7 @@ backgroundMask1      = (H_E_chromatic_highSat2)| black_background;
 backgroundMask_L     = bwlabel(1-backgroundMask1);
 backgroundMask_LP    = regionprops(backgroundMask_L,'area');
 % this returns small regions of white to be included later in innerWhute
-backgroundMask      = backgroundMask1+ismember(backgroundMask_L,find([backgroundMask_LP.Area]<(rows*cols*1e-3)));
+backgroundMask      = backgroundMask1+ismember(backgroundMask_L,find([backgroundMask_LP.Area]<(rows*cols*1e-2)));
 
 
 %% Inner white 
@@ -64,9 +64,28 @@ innerWhite          = innerWhite3;
 innerTissue         = (1-innerWhite3).*backgroundMask4;
 %%
 % Normal is the regions where there is white surrounded by blue/purple
-imagesc(innerWhite3*2+H_E_chromatic_highSat.*(data_hsv(:,:,1)>0.55).*(data_hsv(:,:,1)<0.78))
+blue_purple         = innerTissue.*H_E_chromatic_highSat.*(data_hsv(:,:,1)>0.55).*(data_hsv(:,:,1)<0.75);
+pink_purple         = innerTissue.*H_E_chromatic_highSat.*(data_hsv(:,:,1)>0.75).*(data_hsv(:,:,1)<0.90);
 
+sizeRegion          = 64;
+regionsCombined     = ((innerWhite3*1+2*blue_purple+3*pink_purple) );
+f_G                 = @(block_struct) (sum(sum(block_struct.data==2)))/numel(block_struct.data) * ones(size(block_struct.data));
+f_N                 = @(block_struct) (sum(sum(block_struct.data==1)))/numel(block_struct.data) * ones(size(block_struct.data));
+f_S                 = @(block_struct) (sum(sum(block_struct.data==3)))/numel(block_struct.data) * ones(size(block_struct.data));
+% This defines the normal regions, which are predominantly white but
+% surrounded by pink and blue, thus the percentage has to be 0.4<n<0.8 PLUS
+% it has to be far from the background
+region_N            = blockproc(regionsCombined,[sizeRegion sizeRegion],f_N);
 
+% G3/G4/G5 are easier to define as they have a majority of blue and nothing
+% else so it is G>0.3
+region_G            = blockproc(regionsCombined,[sizeRegion sizeRegion],f_G);
+G345                =region_G>0.3;
+% Stroma regions are those with a high percentage of pink S>0,75, but far from
+% normal previously defined 
+region_S            = blockproc(regionsCombined,[sizeRegion sizeRegion],f_S);
+
+imagesc(innerWhite3*2+blue_purple );
 % % Label to keep large regions
 % data_lowSat_L       = bwlabel(data_lowSat1);
 % data_lowSat_P       = regionprops(data_lowSat1,'area','BoundingBox');
