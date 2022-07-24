ec4097456397e0927331432bb326e9ef3969eb5e
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2022-07-24 22:43:35 +0100

    Update reduceSizeData_TEST.m

--- CODE/reduceSizeData_TEST.m
+++ CODE/reduceSizeData_TEST.m
@@ -3,16 +3,16 @@ clear all
 close all
 cd ('C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\Gleason\CODE')
 %%
-setSelected                 = 1;
+setSelected                 = 3;
 baseDir                     = 'C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\Gleason\DataTest\';
 saveDir                     = 'C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\Gleason\DataTestR\';
-dir0                        = dir(strcat(baseDir,'Subset1*.tiff'));
+dir0                        = dir(strcat(baseDir,'Subset2*.tiff'));
 
 numFiles                    = length (dir0);
 
 %%
 %currentFile                 = 18;
-step                        = 4;
+%step                        = 4;
 for currentFile=1:numFiles
     fileName                    = dir0(currentFile).name;
     %fileName                    = strcat('Subset',num2str(setSelected),'_Train_',num2str(fileName_number(currentFile)),'.tiff');
@@ -21,6 +21,11 @@ for currentFile=1:numFiles
     disp(currentImageName)
     cols                        = currentImageInfo.Width;
     rows                        = currentImageInfo.Height;
+    if (rows>30000)|(cols>30000)
+        step = 2;
+    else
+        step = 1;
+    end     
     currentImage                = imread(currentImageName);
     currentImageR               = currentImage(1:step:end,1:step:end,:);
     save( strcat(saveDir,fileName(1:end-5),'_R.mat'),'currentImageR','-v7.3')
